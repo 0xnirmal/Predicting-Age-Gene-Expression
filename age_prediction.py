99a6ace2df8ec96f5241475214ba4c51c29955f9
Nirmal Krishnan <nkrishn9@jhu.edu>
2017-10-18 13:58:45 -0400

    cleaned up python

--- age_prediction.py
+++ age_prediction.py
@@ -1,8 +1,4 @@
-
 # coding: utf-8
-
-# In[499]:
-
 import numpy as np
 import pandas as pd
 import sys
@@ -26,8 +22,6 @@ cur_path = os.getcwd()
 # * Tranpose the matrices so that rows are patients and columns are gene expression features
 # * We then construct the labels for each tissue. For the age column in our file, we are given 10-year ranges of age for each patient (for example 60-69). I chose to represent an age range as an integer value of the first digit in the range. So for the range 60-69, I encoded this as 6, since we cannot infer anything else. I am choosing to represent age as a continuous value rather than a classification label because there is inherently a hierarchical structure to age. The age range 70-79 not only communicates belonging to class, but it also communicates that it is greater than the class 60-69 and less than the class 80-89. For this reason, it only makes sense to model the problem as a regression.
 # * The GTEx data is already normalized, so this step is not performed here.
-
-# In[500]:
 
 # File parsing
 parser = argparse.ArgumentParser(description='Process display arguments')
@@ -54,8 +48,6 @@ thyroid = gen_matrix(args.thyroid_file)
 whole_blood = gen_matrix(args.whole_blood_file) 
 
 
-# In[501]:
-
 # Construct label sets for each tissue
 labels = pd.read_csv(args.label_file, header=0, sep='\t', dtype=str)
 labels = labels.set_index("SUBJID").drop(["GENDER", "DTHHRDY"], axis=1)
@@ -71,8 +63,6 @@ whole_blood_labels = labels.loc[whole_blood.index].copy()
 # Data Processing
 # * Perform the training-testing split. 
 # * Select top 1000 features based on univariate correlations (f-regression)
-
-# In[512]:
 
 # Feature selection--solely based on training data and labels
 def select_features(data_tup):
@@ -92,8 +82,6 @@ whole_blood_train, whole_blood_test, whole_blood_labels_train, whole_blood_label
 # CV Hyperparameter tuning
 # * Return the optimal lambda on the cv testing over the training data. Uses leave one out. 
 
-# In[503]:
-
 def get_lambda(df, labels):
     regr_cv = linear_model.RidgeCV(alphas=[0.1, 1.0, 10.0, 100], store_cv_values=True)
     regr_cv.fit(df, labels)
@@ -109,16 +97,12 @@ whole_blood_lambda = get_lambda(whole_blood_train, whole_blood_labels_train)
 # * Predict over increments of training examples (of size 10)
 # * Use RMSE to record accuracy
 
-# In[504]:
-
 num_examples = range(50, 300, 10)
 tissues = ["adipose", "muscle", "thyroid", "whole_blood"]
 results = pd.DataFrame(index=tissues, columns=num_examples)
 results.index.name = "Tissue"
 results.columns.name = "Number of Training Examples"
 
-
-# In[505]:
 
 adipose_model = linear_model.Ridge(alpha=adipose_lambda)
 muscle_model = linear_model.Ridge(alpha=muscle_lambda)
@@ -144,15 +128,11 @@ for i in num_examples:
     
 
 
-# In[506]:
-
 ax = results.transpose()["muscle"].plot()
 ax.set_title("Figure 1: RMSE (on Muscle Skeletal Testing Set)")
 ax.set_ylabel("RMSE")
 plt.savefig(cur_path+"/results/figure_1.pdf", format="pdf")
 
-
-# In[507]:
 
 ax = results.transpose().plot()
 ax.set_title("Figure 1A: RMSE (on all Tissues)")
@@ -160,13 +140,9 @@ ax.set_ylabel("RMSE")
 plt.savefig(cur_path+"/results/figure_1A.pdf", format="pdf")
 
 
-# In[508]:
-
 b_results = pd.DataFrame(index=tissues, columns=["total_samples", "smallest_same_number_samples"])
 common_num_samples = thyroid_train.shape[0]
 
-
-# In[509]:
 
 adipose_model.fit(adipose_train, adipose_labels_train)
 adipose_pred = adipose_model.predict(adipose_test)
@@ -201,19 +177,11 @@ whole_blood_pred = whole_blood_model.predict(whole_blood_test)
 b_results.at["whole_blood", "smallest_same_number_samples"] = math.sqrt(mean_squared_error(whole_blood_labels_test, whole_blood_pred))
 
 
-# In[519]:
-
 ax = b_results.plot(kind='bar')
 ax.set_title("Figure 2: RMSE vs. Tissues w/ varrying training setups")
 ax.set_ylabel("RMSE")
 plt.legend(bbox_to_anchor=(0.25, 1.05))
-#plt.gcf().subplots_adjust(bottom=0.30)
-#plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3, ncol=2, mode="expand", borderaxespad=0.)
 plt.tight_layout()
 plt.savefig(cur_path+"/results/figure_2.pdf", format="pdf")
-
-
-# In[ ]:
-
 
 
